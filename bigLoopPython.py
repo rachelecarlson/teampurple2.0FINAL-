9c17d3d5c9a54b73c47c502599db143a71eb3459
acherry4 <alaina_cherry@brown.edu>
2021-04-04 16:09:44 -0400

    Continuint to translate - 45 minutes

--- bigLoopPython.py
+++ bigLoopPython.py
@@ -7,109 +7,109 @@ import matplotlib as mpl
 
 
 counter = 1
-disp('Instructions: In each round, two colored grids will be displayed (one after the other). When prompted, indicate whether the two grids were the same or different.')
+print('Instructions: In each round, two colored grids will be displayed (one after the other). When prompted, indicate whether the two grids were the same or different.')
 answer_vector = []
 groundtruth = [] #keeping track of whether the grid is disharmonious or harmonious
 samedifferent = [] #keeping track of whether the grid was the same or different in the trial
 while counter <=5: #the program will run for 5 rounds 
-    blankMat = zeros(150)
-    imshow(uint8(blankMat))
+    blankMat = np.zeros(150)
+    imshow(np.uint8(blankMat))
     pause(3)
-    T = randi([1,2],1)
-    groundtruth = [groundtruth T]
+    T = randint([1,2],1)
+    groundtruth = [groundtruth T] #trying to concatonate 
     if T == 1:
-        c = randi(3) %harmonious grid 
-        c = uint8(c)
-        yy = zeros(150,150,3)
-        yy= uint8(yy)
-        sat = randi(255)
-        sat = uint8(sat)
-        sat2 = randi(255)
-        sat2 = uint8(sat2)
-        sat3 = randi(255)
-        sat3= uint8(sat3)
-        sat4 = randi(255)
-        sat4= uint8(sat4)
-        sat5 = randi(255)
-        sat5= uint8(sat5)
-        sat6 = randi(255)
-        sat6= uint8(sat6)
-        sat7 = randi(255)
-        sat7= uint8(sat7)
-        sat8 = randi(255)
-        sat8= uint8(sat8)
-        sat9 = randi(255)
-        sat9= uint8(sat9)
+        c = randint(3) %harmonious grid 
+        c = np.uint8(c)
+        yy = np.zeros(150,150,3)
+        yy= np.uint8(yy)
+        sat = randint(255)
+        sat = np.uint8(sat)
+        sat2 = randint(255)
+        sat2 = np.uint8(sat2)
+        sat3 = randint(255)
+        sat3= np.uint8(sat3)
+        sat4 = randint(255)
+        sat4= np.uint8(sat4)
+        sat5 = randint(255)
+        sat5= np.uint8(sat5)
+        sat6 = randint(255)
+        sat6= np.uint8(sat6)
+        sat7 = randint(255)
+        sat7= np.uint8(sat7)
+        sat8 = randint(255)
+        sat8= np.uint8(sat8)
+        sat9 = randint(255)
+        sat9= np.uint8(sat9)
         
         #top left lego
-        for current_row = 1:50
-            for current_col = 1:50
+        for current_row in range(0,50)
+            for current_col in range (0,50)
                 yy(current_row,current_col, c) = sat:
       
         
         #top middle lego
-        for current_row = 1:50
-            for current_col = 51:100
+        for current_row in range (0,50)
+            for current_col in range (50,100)_
                 yy(current_row,current_col, c) = sat2
    
 
         #top right lego
-        for current_row = 1:50
-            for current_col = 101:150
+        for current_row in range (0,50)
+            for current_col in range(100:150)
                 yy(current_row,current_col, c) = sat3
 
         
         #middle left lego
-        for current_row = 51:100
-            for current_col = 1:50
+        for current_row in range(50:100)
+            for current_col in range (0:50)
                 yy(current_row,current_col, c) = sat4
 
 
         
         #center lego
-        for current_row = 51:100
-            for current_col = 51:100
+        for current_row in range(50,100)
+            for current_col in range(50:100)
                 yy(current_row,current_col, c) = sat5
 
   
         #middle right
-        for current_row = 51:100
-            for current_col = 101:150
+        for current_row in range(50,100)
+            for current_col in range(100,150)
                 yy(current_row,current_col, c) = sat6
         
         #bottom left
-        for current_row = 101:150
-            for current_col = 1:50
+        for current_row in range(100,150)
+            for current_col in range (1,50)
                 yy(current_row,current_col, c) = sat7
         
         #bottom middle lego
-        for current_row = 101:150
-            for current_col =51:100
+        for current_row in range(100,150)
+            for current_col in range(50,100)
                 yy(current_row,current_col, c) = sat8
 
 
         
         #bottom right lego
-        for current_row = 101:150
-            for current_col = 101:150
+        for current_row in range(100:150)
+            for current_col in range(100:150)
                 yy(current_row,current_col, c) = sat9
 
         
         
-        imshow(uint8(yy)) %we are showing harmious grid
+        imshow(np.uint8(yy)) #we are showing harmious grid
         pause(3)
         
-        blankMat = zeros(150)
-        imshow(uint8(blankMat))
+        blankMat = np.zeros(150)
+        imshow(np.uint8(blankMat))
         pause(3)
-        
+       
         #Blank screen
-        R = randi([3,4],1)
+        R = randint([3,4],1)
         samedifferent = [samedifferent, R]
         if R == 3: #if we want to show the exact same grid
-            imshow(uint8(yy)) #showing same grid
-            blankMat = zeros(150)
-            imshow(uint8(blankMat))
+            imshow(np.uint8(yy)) #showing same grid
+            blankMat = np.zeros(150)
+            imshow(np.uint8(blankMat))
             pause(3)
             
             
@@ -119,219 +119,219 @@ while counter <=5: #the program will run for 5 rounds
             #harmonious funky lego
             HG = yy
             
-            rand_row = randi(3)
-            rand_col = randi(3)
+            rand_row = randint(3)
+            rand_col = randint(3)
             
             if rand_row == 1 and rand_col == 1:
-                for current_row = 1:50
-                    for current_col = 1:50
+                for current_row in range(0,50):
+                    for current_col in range(0,50):
                         HG(current_row,current_col, c) = sat
 
-            elif rand_row == 1 and rand_col == 2
-                for current_row = 1:50
-                    for current_col = 51:100
+            elif rand_row == 1 and rand_col == 2:
+                for current_row in range(0,50):
+                    for current_col in range(50,100):
                         HG(current_row,current_col, c) = sat
  
-            elif rand_row == 1 and rand_col == 3
-                for current_row = 1:50
-                    for current_col = 101:150
+            elif rand_row == 1 and rand_col == 3:
+                for current_row in range(0,50):
+                    for current_col in range(100,150):
                         HG(current_row,current_col, c) = sat
 
-            elif rand_row == 2 and rand_col == 1
-                for current_row = 51:100
-                    for current_col = 1:50
+            elif rand_row == 2 and rand_col == 1:
+                for current_row in range(50,100):
+                    for current_col in range(0,50):
                         HG(current_row,current_col, c) = sat
                        
-            elif rand_row == 2 and rand_col == 2
-                for current_row = 51:100
-                    for current_col = 51:100
+            elif rand_row == 2 and rand_col == 2:
+                for current_row in range(50,100):
+                    for current_col in range(50,100):
                         HG(current_row,current_col, c) = sat
 
-            elif rand_row == 2 and rand_col == 3
-                for current_row = 51:100
-                    for current_col = 101:150
+            elif rand_row == 2 and rand_col == 3:
+                for current_row in range(50,100):
+                    for current_col in range(100,150):
                         HG(current_row,current_col, c) = sat
                        
-            elif rand_row == 3 and rand_col == 1
-                for current_row = 101:150
-                    for current_col = 1:50
+            elif rand_row == 3 and rand_col == 1:
+                for current_row in range(100,150):
+                    for current_col in range(0,50):
                         HG(current_row,current_col, c) = sat
 
-            elif rand_row == 3 and rand_col == 2
-                for current_row = 101:150
-                    for current_col = 51:100
+            elif rand_row == 3 and rand_col == 2:
+                for current_row in range(100,150):
+                    for current_col in range(50,100):
                         HG(current_row,current_col, c) = sat
 
-            elseif rand_row == 3 and rand_col == 3
-                for current_row = 101:150
-                    for current_col = 101:150
+            elif rand_row == 3 and rand_col == 3:
+                for current_row in range(100,150):
+                    for current_col in range(100:150)
                         HG(current_row,current_col, c) = sat
   
             
-            imshow(uint8(HG)) #showing harmonious funky lego
+            imshow(np.uint8(HG)) #showing harmonious funky lego
             pause(3)
             
-            blankMat = zeros(150)
-            imshow(uint8(blankMat))
+            blankMat = np.zeros(150)
+            imshow(np.uint8(blankMat))
             pause(3)
             
             
          #end for if statement within big if statement
     else:
-        c = randi(3) #%% disharmonious grid
-        c = uint8(c)
-        cc = randi(3)
+        c = randint(3) #%% disharmonious grid
+        c = np.uint8(c)
+        cc = randint(3)
         if cc == c and c == 1:
-            cc = randi([2, 3], 1)
+            cc = randint([2, 3], 1)
         elif cc == c and c == 2:
-            cc = randi([1, 3], 1)
+            cc = randint([1, 3], 1)
         elif cc==c and c == 3:
-            cc = randi([1, 2], 1)
+            cc = randint([1, 2], 1)
         else
         #end 
 
         xx = zeros(150,150,3)
-        xx= uint8(xx)
-        sat = randi(255)
-        sat = uint8(sat)
-        sat2 = randi(255)
-        sat2 = uint8(sat2)
-        sat3 = randi(255)
-        sat3= uint8(sat3)
-        sat4 = randi(255)
-        sat4= uint8(sat4)
-        sat5 = randi(255)
-        sat5= uint8(sat5)
-        sat6 = randi(255)
-        sat6= uint8(sat6)
-        sat7 = randi(255)
-        sat7= uint8(sat7)
-        sat8 = randi(255)
-        sat8= uint8(sat8)
-        sat9 = randi(255)
-        sat9= uint8(sat9)
+        xx= np.uint8(xx)
+        sat = randint(255)
+        sat = np.uint8(sat)
+        sat2 = randint(255)
+        sat2 = np.uint8(sat2)
+        sat3 = randint(255)
+        sat3= np.uint8(sat3)
+        sat4 = randint(255)
+        sat4= np.uint8(sat4)
+        sat5 = randint(255)
+        sat5= np.uint8(sat5)
+        sat6 = randint(255)
+        sat6= np.uint8(sat6)
+        sat7 = randint(255)
+        sat7= np.uint8(sat7)
+        sat8 = randint(255)
+        sat8= np.uint8(sat8)
+        sat9 = randint(255)
+        sat9= np.uint8(sat9)
         
         #top left lego
-        for current_row = 1:50:
-            for current_col = 1:50:
+        for current_row in range(0,50):
+            for current_col in range(0,50):
                 xx(current_row,current_col, c) = sat
         
         #top middle lego
-        for current_row = 1:50:
-            for current_col = 51:100:
+        for current_row in range(0:50):
+            for current_col in range(50:100):
                 xx(current_row,current_col, c) = sat2
         
         #top right lego
-        for current_row = 1:50:
-            for current_col = 101:150:
+        for current_row in range(0,50):
+            for current_col in range(100,150):
                 xx(current_row,current_col, cc) = sat3
                
         
         #middle left lego
-        for current_row = 51:100
-            for current_col = 1:50
+        for current_row in range(50,100):
+            for current_col in range(0,50):
                 xx(current_row,current_col, c) = sat4
             
         
         #center lego
-        for current_row = 51:100:
-            for current_col = 51:100:
+        for current_row in range(50,100):
+            for current_col in range(50,100):
                 xx(current_row,current_col, c) = sat5
               
         
         #middle right
-        for current_row = 51:100:
-            for current_col = 101:150:
+        for current_row in range(50,100):
+            for current_col in range(100,150):
                 xx(current_row,current_col, cc) = sat6
               
         
         #bottom left
-        for current_row = 101:150:
-            for current_col = 1:50:
+        for current_row in range(100,150):
+            for current_col in range(1,50):
                 xx(current_row,current_col, cc) = sat7
                
         
         #bottom middle lego
-        for current_row = 101:150:
-            for current_col =51:100:
+        for current_row in range(100,150):
+            for current_col in range(50,100):
                 xx(current_row,current_col, c) = sat8
               
         
         #bottom right lego
-        for current_row = 101:150:
-            for current_col = 101:150:
+        for current_row in range(100,150):
+            for current_col in range(100,150):
                 xx(current_row,current_col, cc) = sat9
         
         
         
-        imshow(uint8(xx)) %showing disharmonious grid
+        imshow(np.uint8(xx)) %showing disharmonious grid
         pause(3)
         
-        blankMat = zeros(150)
-        imshow(uint8(blankMat))
+        blankMat = np.zeros(150)
+        imshow(np.uint8(blankMat))
         pause(3)
         
         #Blank screen
-        Rr = randi([5,6], 1)
+        Rr = randint([5,6], 1)
         if Rr == 5: #if we want to keep the same disharmonious grid
-            imshow(uint8(xx)) % showing same disharmonious grid
+            imshow(np.uint8(xx)) % showing same disharmonious grid
         else #disharmonious funky lego code
             
             DG = xx
             
-            rand_row = randi(3)
-            rand_col = randi(3)
+            rand_row = randint(3)
+            rand_col = randint(3)
             
             if rand_row == 1 and rand_col == 1:
-                for current_row = 1:50:
-                    for current_col = 1:50:
+                for current_row in range(0,50):
+                    for current_col in range(0,50):
                         DG(current_row,current_col, cc) = sat
                         
             elif rand_row == 1 and rand_col == 2:
-                for current_row = 1:50:
-                    for current_col = 51:100:
+                for current_row in range(0,50):
+                    for current_col in range(50,100):
                         DG(current_row,current_col, cc) = sat
                        
             elif rand_row == 1 and rand_col == 3:
-                for current_row = 1:50:
-                    for current_col = 101:150:
+                for current_row in range(0,50):
+                    for current_col in range(100,150):
                         DG(current_row,current_col, cc) = sat
                        
             elif rand_row == 2 and rand_col == 1:
-                for current_row = 51:100:
-                    for current_col = 1:50:
+                for current_row in range(50,100):
+                    for current_col in range(0,50):
                         DG(current_row,current_col, cc) = sat
                       
             elif rand_row == 2 and rand_col == 2:
-                for current_row = 51:100:
-                    for current_col = 51:100:
+                for current_row in range(50,100):
+                    for current_col in range(50,100):
                         DG(current_row,current_col, cc) = sat
                     
             elif rand_row == 2 and rand_col == 3:
-                for current_row = 51:100:
-                    for current_col = 101:150:
+                for current_row in range(50,100):
+                    for current_col in range(100,150)
                         DG(current_row,current_col, cc) = sat
                         
             elif rand_row == 3 and rand_col == 1:
-                for current_row = 101:150:
-                    for current_col = 1:50:
+                for current_row in range(100,150):
+                    for current_col in range(0,50):
                         DG(current_row,current_col, cc) = sat
                         
             elif rand_row == 3 and rand_col == 2:
-                for current_row = 101:150:
-                    for current_col = 51:100:
+                for current_row in range(100,150):
+                    for current_col in range(50,100):
                         DG(current_row,current_col, cc) = sat
                        
             elif rand_row == 3 and rand_col == 3:
-                for current_row = 101:150:
-                    for current_col = 101:150:
+                for current_row in range(100,150)
+                    for current_col in range(100,150):
                         DG(current_row,current_col, cc) = sat
                         
-            imshow(uint8(DG)) #showing disharmonious funky lego
+            imshow(np.uint8(DG)) #showing disharmonious funky lego
             pause(3)
             
-            blankMat = zeros(150)
-            imshow(uint8(blankMat))
+            blankMat = np.zeros(150)
+            imshow(np.uint8(blankMat))
             pause(3)
             
 
