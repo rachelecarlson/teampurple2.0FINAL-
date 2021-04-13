21aa42209bb493c8ceba74cabc31ee7f01ce7d60
rachelecarlson <rachel_carlson@brown.edu>
2021-04-13 12:00:58 -0400

    WORKING harmonious grid (and harmonious grid with one different block) 1 hr 15 min

--- gridTrials .py
+++ gridTrials .py
@@ -5,6 +5,7 @@ import pandas as pd
 from skimage.color import rgb2hsv, hsv2rgb 
 import matplotlib.pyplot as mplpy 
 import cv2
+from copy import deepcopy
 
 numBlocks = 3 #number of blocks in rows or columns (so n = 3 is a 3x3 grid) 
 gridSize = 150 #overall pixels in grid 
@@ -28,65 +29,79 @@ while counter <= 5: #the program will run for 5 rounds
     
     trialType = np.random.randint(GridType) #if trial type =1 harmonious, if 2, disharmonious
     groundtruth.append(trialType) #trying to concatonate
-    trialType = 1 #?
+    trialType = 1 #right now, just shortcut for it to be 1 -- once we have code for trialType = 2 we can delete
+
+    #import ipdb; ipdb.set_trace()
 
     if trialType == 1: # harmonious -----------------------------
         if useHSV: 
             randomHue = np.random.rand() #for harmonious grid (picking one color channel and random int within that color)
             # c should give number between 0 and 1 for HUE
-            for satLoop in range(0, numBlocks):
-                numBlocks[satLoop] = np.random.randint(255, size=(numBlocks,numBlocks))
-                randomSat = numBlocks ##for saturation 
-            randomValue = np.random.rand()
-
-            resizedChannel = cv2.resize(randomSat.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
+            #for satLoop in range(0, numBlocks):
+                #numBlocks[satLoop] = np.random.randint(255, size=(numBlocks,numBlocks))
+                #randomSat = numBlocks
+            randomSat = np.random.rand(numBlocks,numBlocks)
+            #randomSat creates a numB by numBlocks matrix, filled with values from 0 to 1  
+            randomValue =  0.2 * np.random.rand() + 0.8 #making sure that the value is never 0
+            #resizedChannel = cv2.resize(randomSat.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
                 #interpolation has to due with the way the blocks blend together, so interpolation=cv2.INTER_NEAREST_EXACT means 
                 #that the blocks are distinct (not tye-dye looking)
-            displayImage = np.zeros((gridSize,gridSize,3), dtype=np.float32) #the 3 is for color channels 
-            displayImage[:,:,0] += randomHue 
-            displayImage[:,:,1] = resizedChannel
+            displayImage = np.zeros((numBlocks,numBlocks,3), dtype=np.float32) #the 3 is for color channels 
+            displayImage[:,:,0] += randomHue #+= because randomHue is a single number, not a matrix
+            displayImage[:,:,1] = randomSat #randomSat is a matrix 
             displayImage[:,:,2] += randomValue
     
-            RGBImage = hsv2rgb(displayImage)
+            RGBImage = hsv2rgb(displayImage) * 255
+            RGBImage = cv2.resize(RGBImage.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
+                
         else: 
             pass 
 
 
         mplpy.imshow(RGBImage)
-        mplpy.show()
+        mplpy.axis('off')
+        mplpy.pause(3)
        
         mplpy.imshow(blankMat)
-        mplpy.show()
+        mplpy.axis('off')
+        mplpy.pause(3)
 
-        pause(3)
         GridSD = np.random.randint(nTrialTypes)
-        GridSD = 1
         samedifferent.append(GridSD)
-        if GridSD == 3: #Show the same harmonious grid as user just saw -----------------------
-            mplpy.imshow(yy) #showing same grid
-            mplpy.show() 
-            pause(3)
 
-            blankMat = np.zeros(150)
+        if GridSD == 0: #Show the same harmonious grid as user just saw -----------------------
+            print('Same')
+            mplpy.imshow(RGBImage) #showing same grid
+            mplpy.axis('off')
+            mplpy.pause(3)
+
             mplpy.imshow(blankMat)
-            mplpy.show()
-            pause(3)
+            mplpy.axis('off')
+            mplpy.pause(3)
 
-        else: #if GridSD == 4, show the original harmonious grid, with one lego different  
-            #two parenthesis, because we want it to know that it's a tuple (two separate entities)
+        else: #if GridSD == 1, show the original harmonious grid, with one lego different  
             #attempting more sustainable code, using matrix instead of hard code 
             #zz = np.random.randint(255, size=(numBlocks,numBlocks)) ##for saturation 
-            funkyLegoLen = np.random.randint(0,gridSize+1,size=None, dType=np.uint8)
-            funkyLegoWid = np.random.randint(0,gridSize+1,size=None, dType=np.uint8)
-            FunkyLegoGrid = yy
-            FunkyLegoGrid[funkyLegoLen,funkyLegoWid,1] = np.random.rand()
-            #1 is the HUE... np.random.rand() 
-            resizedChannel = cv2.resize(FunkyLegoGrid.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
-            #interpolation has to due with the way the blocks blend together, so interpolation=cv2.INTER_NEAREST_EXACT means 
-            #that the blocks are distinct (not tye-dye looking)
-            #yy[:,:,c] = resizedChannel
+            print('Different')
+            funkyLegoX = np.random.randint(numBlocks)
+            funkyLegoY = np.random.randint(numBlocks)
+            funkyLegoGrid = deepcopy(displayImage)
 
-    #if trialType == 2:
+            # change saturation for a randomly selected pixel
+            funkyLegoGrid[funkyLegoX, funkyLegoY, 1] = np.random.rand()
+            funkyLegoGrid = hsv2rgb(funkyLegoGrid) * 255  
+            funkyLegoGrid = cv2.resize(funkyLegoGrid.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
+             
+            mplpy.imshow(funkyLegoGrid) #showing same grid
+            mplpy.axis('off')
+            mplpy.pause(3)
+
+            mplpy.imshow(blankMat)
+            mplpy.axis('off')
+            mplpy.pause(3)
+
+    elif trialType == 2:
+        pass
 
 
 
