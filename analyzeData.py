ec928e223522d1c21efa24b1dd8fd6222d33bc8b
rachelecarlson <rachel_carlson@brown.edu>
2021-04-19 19:46:42 -0400

    editing heapmat aesthetics

--- analyzeData.py
+++ analyzeData.py
@@ -4,11 +4,15 @@ import pickle
 import matplotlib as mpl 
 import matplotlib.pyplot as mplpy
 import numpy as np
+import seaborn as sb
+from matplotlib import colors 
+
 
 
 confusionMatrix = np.zeros((2,2)) #data for each participant 
 totalTrials = np.zeros((2,2)) #KEEPING TRACK OF TOTAL # OF TRIALS 
 #2x2 since we have rows harm or disharm, and columns same or different 
+
 
 for subjectID in range(7): #if we have 7 participants 
     data = pickle.load(open('subject_{}.p'.format(subjectID), 'rb')) #reading (rb)
@@ -17,28 +21,31 @@ for subjectID in range(7): #if we have 7 participants
     totalTrials += data['total']
 
 confusionMatrix = confusionMatrix/totalTrials
+confusionMatrix = np.around(confusionMatrix, decimals=3)
 #measures accuracy 
 
 
 #x labels s/d, y labels h/d  
 
 fig, ax = mplpy.subplots()
-im = ax.imshow(confusionMatrix)
+im = ax.imshow(confusionMatrix, cmap = 'YlOrRd')
 
+ 
 sameDiff = ["Same", "Different"]
 harmDisharm = ["Harmonious", "Disharmonious"]
-
+ 
 ax.set_xticks(np.arange(len(sameDiff)))
 ax.set_yticks(np.arange(len(harmDisharm)))
-ax.set_xticklabels(sameDiff, fontsize=10) 
-ax.set_yticklabels(harmDisharm, fontsize=10)
+ax.set_xticklabels(sameDiff, fontsize=12) 
+ax.set_yticklabels(harmDisharm, fontsize=12)
 mplpy.setp(ax.get_xticklabels(), rotation=0, ha="right", rotation_mode="anchor")
 
 
 for i in range(len(harmDisharm)):
     for j in range(len(sameDiff)):
-        text = ax.text(j,i, confusionMatrix[i,j], ha = "center", va ="center", color="b")
+        text = ax.text(j,i, confusionMatrix[i,j], ha = "center", va ="center", color="k", fontsize = 18)
 
 ax.set_title("Participant Accuracy by Trial Type in Color Relations Experiment", fontweight = 'bold', fontsize=16)
 fig.tight_layout()
 mplpy.show()
+
