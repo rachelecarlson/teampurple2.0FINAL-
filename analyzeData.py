cb7e011312561c0d69614af207ca533739e1b78d
rachelecarlson <rachel_carlson@brown.edu>
2021-04-21 12:55:59 -0400

    commenting to make more clear for users :)

--- analyzeData.py
+++ analyzeData.py
@@ -11,10 +11,10 @@ from matplotlib import colors
 
 confusionMatrix = np.zeros((2,2)) #data for each participant 
 totalTrials = np.zeros((2,2)) #KEEPING TRACK OF TOTAL # OF TRIALS 
-#2x2 since we have rows harm or disharm, and columns same or different 
+#2x2 since we have rows harm or disharm, and columns are either same or different 
 
 
-for subjectID in range(7): #if we have 7 participants 
+for subjectID in range(6): #if we have 6 participants 
     data = pickle.load(open('subject_{}.p'.format(subjectID), 'rb')) #reading (rb)
     confusionMatrix += data['confmat'] #accessing the values we've saved in gridTrials file 
     #collapsing data across all participants 
@@ -25,7 +25,7 @@ confusionMatrix = np.around(confusionMatrix, decimals=3)
 #measures accuracy 
 
 
-#x labels s/d, y labels h/d  
+#x labels same/different, y labels harmonious/disharmonious  
 
 fig, ax = mplpy.subplots()
 im = ax.imshow(confusionMatrix, cmap = 'YlOrRd')
@@ -40,7 +40,7 @@ ax.set_xticklabels(sameDiff, fontsize=12)
 ax.set_yticklabels(harmDisharm, fontsize=12)
 mplpy.setp(ax.get_xticklabels(), rotation=0, ha="right", rotation_mode="anchor")
 
-
+#fill in the grid with data from confusionMatrix 
 for i in range(len(harmDisharm)):
     for j in range(len(sameDiff)):
         text = ax.text(j,i, confusionMatrix[i,j], ha = "center", va ="center", color="k", fontsize = 18)
--- gridTrials .py
+++ gridTrials .py
@@ -6,55 +6,55 @@ import cv2
 from copy import deepcopy
 import pickle   
 
-numBlocks = 3 #number of blocks in rows or columns (so n = 3 is a 3x3 grid) 
-gridSize = 200 #overall pixels in grid 
+numBlocks = 3 #number of blocks in rows or columns (so numBlocks = 3 is a 3x3 grid) 
+gridSize = 200 #overall pixels in the grid 
 GridType = 2 #there are two options for grids––same grid or different grid
 nTrialTypes = 2 #there are two options for trial types––harmonious or disharmonious
 useHSV = True #flag that will indicate if you want to work in HSV or RGB space 
-subjectID = 6 #each time you run a different perosn, update this :) 
+subjectID = 6 #each time you run a different person, update this :) 
 
 
 counter = 0
 print('Instructions: In each round, two colored grids will be displayed (one after the other). When prompted, indicate whether the two grids were the same or different.')
-UserAnswerList = []
+UserAnswerList = [] #tracks users answers after each round (same or different)
 CorrectHarmDisharmList = [] #keeping track of whether the grid is disharmonious or harmonious
 CorrectAnswerList = [] #keeping track of whether the grid was the same or different in the trial
 
 while counter <= 9: #the program will run for 10 rounds 
     counter += 1
-    blankMat = np.zeros((gridSize,gridSize))
-    #mplpy.imshow(blankMat.astype(np.uint8))
-    #mplpy.pause(3)
+    blankMat = np.zeros((gridSize,gridSize)) #blank matrix to fill later 
     
-    trialType = np.random.randint(GridType) #if trial type =1 harmonious, if 2, disharmonious
-    CorrectHarmDisharmList.append(trialType) #trying to concatonate
-    #trialType = 1 #right now, just shortcut for it to be 1 -- once we have code for trialType = 2 we can delete
+    trialType = np.random.randint(GridType) #if trial type =0 harmonious, if 1, disharmonious
+    CorrectHarmDisharmList.append(trialType) #keeps track of whether grid was harmonious or disharm
 
-    #import ipdb; ipdb.set_trace()
 
-    if trialType == 0: # harmonious -----------------------------
-        #print('harmonious')
+    if trialType == 0: # harmonious grid 
+        #print('harmonious') 
+        # print the above if you want to keep track of what is being shown throughout the trial (only for researcher use, not participant)
         if useHSV: 
             randomHue = np.random.rand() #for harmonious grid (picking one color channel and random int within that color)
             # c should give number between 0 and 1 for HUE
-            #for satLoop in range(0, numBlocks):
-                #numBlocks[satLoop] = np.random.randint(255, size=(numBlocks,numBlocks))
-                #randomSat = numBlocks
+
             randomSat = np.random.rand(numBlocks,numBlocks)
             #randomSat creates a numB by numBlocks matrix, filled with values from 0 to 1  
-            randomValue =  0.2 * np.random.rand() + 0.8 #making sure that the value is never 0
-            #resizedChannel = cv2.resize(randomSat.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
-                #interpolation has to due with the way the blocks blend together, so interpolation=cv2.INTER_NEAREST_EXACT means 
-                #that the blocks are distinct (not tye-dye looking)
+            randomValue =  0.2 * np.random.rand() + 0.8 
+            #making sure that the value is never 0
+        
             displayImage = np.zeros((numBlocks,numBlocks,3), dtype=np.float32) #the 3 is for color channels 
-            displayImage[:,:,0] += randomHue #+= because randomHue is a single number, not a matrix
+            
+            displayImage[:,:,0] += randomHue 
+            #filling displayImage with random hue 
+            #+= because randomHue is a single number, not a matrix 
             displayImage[:,:,1] = randomSat #randomSat is a matrix 
+            #fill display image with random sat within the same Hue value as was chosen above 
             displayImage[:,:,2] += randomValue
-    
+
+
+            #image created in HSV, but must be shown in RGB 
             RGBImage = hsv2rgb(displayImage) * 255
             RGBImage = cv2.resize(RGBImage.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
                 
-        else: 
+        else: #in the future, to add RGB option for users 
             pass 
 
 
@@ -62,14 +62,15 @@ while counter <= 9: #the program will run for 10 rounds
         mplpy.axis('off')
         mplpy.pause(3)
        
+       #show blankMat between rounds
         mplpy.imshow(blankMat)
         mplpy.axis('off')
         mplpy.pause(3)
 
-        GridSD = np.random.randint(nTrialTypes)
+        GridSD = np.random.randint(nTrialTypes) #keeping track of same/different 
         CorrectAnswerList.append(GridSD)
 
-        if GridSD == 0: #Show the same harmonious grid as user just saw -----------------------
+        if GridSD == 0: #Show the same harmonious grid as user just saw 
             #print('Harmonious Same')
             mplpy.imshow(RGBImage) #showing same grid
             mplpy.axis('off')
@@ -80,14 +81,14 @@ while counter <= 9: #the program will run for 10 rounds
             mplpy.pause(3)
 
         else: #if GridSD == 1, show the original harmonious grid, with one lego different  
-            #attempting more sustainable code, using matrix instead of hard code 
-            #zz = np.random.randint(255, size=(numBlocks,numBlocks)) ##for saturation 
             #print('Harmonious Different')
+
+            #funkyLegoX and Y pick a random grid-unit to change 
             funkyLegoX = np.random.randint(numBlocks)
             funkyLegoY = np.random.randint(numBlocks)
             funkyLegoGrid = deepcopy(displayImage)
 
-            # change saturation for a randomly selected pixel
+            # change saturation for the randomly selected pixel
             funkyLegoGrid[funkyLegoX, funkyLegoY, 1] = np.random.rand()
             funkyLegoGrid = hsv2rgb(funkyLegoGrid) * 255  
             funkyLegoGrid = cv2.resize(funkyLegoGrid.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
@@ -100,6 +101,8 @@ while counter <= 9: #the program will run for 10 rounds
             mplpy.axis('off')
             mplpy.pause(3)
         
+        #will appear in command window, for user to input answer
+        #program will pause until the user inputs answer 
         UserInput = input("End of Round! Type 's' if the two grids were the same. Type 'd' if the grids were different. Enter response here: ") 
         if UserInput == 's': 
             UserAnswerList.append(0)
@@ -112,11 +115,10 @@ while counter <= 9: #the program will run for 10 rounds
         if useHSV:
             #print("disharmonious")
             randomHue = np.random.rand(numBlocks,numBlocks) #picking one color channel and random int within that color
-            # c should give number between 0 and 1 for HUE
             randomSat = np.random.rand(numBlocks, numBlocks)
             #randomSat creates a numB by numBlocks matrix, filled with values from 0 to 1  
             randomValue =  0.2 * np.random.rand() + 0.8 #making sure that the value is never 0
-            #resizedChannel = cv2.resize(randomSat.astype(np.uint8), (gridSize,gridSize), interpolation=cv2.INTER_NEAREST_EXACT)
+        
             displayImage = np.zeros((numBlocks,numBlocks,3), dtype=np.float32) #the 3 is for color channels 
             displayImage[:,:,0] = randomHue #+= because randomHue is a single number, not a matrix
             displayImage[:,:,1] = randomSat #randomSat is a matrix 
@@ -140,7 +142,7 @@ while counter <= 9: #the program will run for 10 rounds
         GridSD = np.random.randint(nTrialTypes)
         CorrectAnswerList.append(GridSD)
     
-        if GridSD == 0: #Show the same disharmonious grid as user just saw -----------------------
+        if GridSD == 0: #Show the same disharmonious grid as user just saw 
             #print('Disharmonious Same')
                 
             mplpy.imshow(RGBImage) #showing same grid
@@ -179,16 +181,19 @@ while counter <= 9: #the program will run for 10 rounds
         else: 
             UserAnswerList.append('null')
 
-        
+#these will print user's answers, correct answers, and whether the grids were harm or disharm 
+# can be included in trials if researcher wants immediate feedback, or commented out        
 print(UserAnswerList)   
 print(CorrectAnswerList)     
 print(CorrectHarmDisharmList)
 
-confusionMatrix = np.zeros((2,2)) #data for this participant 
+#data for this participant
+confusionMatrix = np.zeros((2,2))  
 
 totalTrials = np.zeros((2,2)) #KEEPING TRACK OF TOTAL # OF TRIALS 
 #2x2 since we have rows harm or disharm, and columns same or different 
 
+#comparing UserAnswerList (user's answers to the correct answer list)
 for k in range(len(UserAnswerList)):
     isCorrect = UserAnswerList[k] == CorrectAnswerList[k]
     if isCorrect: 
@@ -202,8 +207,6 @@ for k in range(len(UserAnswerList)):
 
 
 print(confusionMatrix)
-#mplpy.imshow(confusionMatrix)
-#mplpy.show()
 
 data = {'confmat': confusionMatrix, 'total':totalTrials}
 #dictionary of confusionMatrix, with two keys (confusionMatrix and totalTrials)
